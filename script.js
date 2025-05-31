1e9a6577ed14e36f320b41644dc830a84cb4fa3e
StephienOledibe <stephienamarachi123@gmail.com>
2025-05-30 23:06:53 -0700

    About me page completed

---
+++ script.js
@@ -0,0 +1,12 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontent = document.getElementsByClassName("tab-content");
+
+// function opentab(tabName){
+//     for (tablink of tablinks) {
+//         tablink.classList.remove("active-link");
+//     }
+//     for (tabcontent of tabcontents) {
+//         tabcontent.classList.remove("active-content");
+//     }
+    
+// }
